7c929f12f072e5ca9fa93648787d758a64c54d51
Shariful Nibir <nibirsharif@gmail.com>
2019-11-24 10:44:16 +0800

    refactored layout and added purchase summary modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
-import Burger from '../../components/Layout/Burger/Burger';
-import BuildControls from '../../components/Layout/Burger/BuildControls/BuildControls';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -13,55 +15,84 @@ const INGREDIENT_PRICES = {
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 1
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false,
+        purchasing: false
     };
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true})
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false })
+    }
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({ purchasable: sum > 0 });
+    }
 
     addIngredientHandler = (type) => {
         const previousCount = this.state.ingredients[type];
         const updatedCount = previousCount + 1;
-        const updatedIngredient = {
+        const updatedIngredients = {
             ...this.state.ingredients
         };
-        updatedIngredient[type] = updatedCount;
+        updatedIngredients[type] = updatedCount;
         const priceAddition = INGREDIENT_PRICES[type];
         const previousPrice = this.state.totalPrice;
         const updatedPrice = previousPrice + priceAddition;
         this.setState({
-            ingredients: updatedIngredient,
+            ingredients: updatedIngredients,
             totalPrice: updatedPrice
         });
+        this.updatePurchaseState(updatedIngredients);
     };
 
     removeIngredientHandler = (type) => {
         const previousCount = this.state.ingredients[type];
         if (previousCount <= 0) return;
         const updatedCount = previousCount - 1;
-        const updatedIngredient = {
+        const updatedIngredients = {
             ...this.state.ingredients
         };
-        updatedIngredient[type] = updatedCount;
+        updatedIngredients[type] = updatedCount;
         const priceDeduction = INGREDIENT_PRICES[type];
         const previousPrice = this.state.totalPrice;
         const updatedPrice = previousPrice - priceDeduction;
         this.setState({
-            ingredients: updatedIngredient,
+            ingredients: updatedIngredients,
             totalPrice: updatedPrice
         });
+        this.updatePurchaseState(updatedIngredients);
     };
-    
+
     render() {
         return (
             <Auxiliary>
+                <Modal show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={this.state.ingredients}
+                    purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     price={this.state.totalPrice} />
             </Auxiliary>
         );

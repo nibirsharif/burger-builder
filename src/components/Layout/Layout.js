f5f3a0e2014c111e5ed444604f7baaa31ae7f551
Shariful Nibir <nibirsharif@gmail.com>
2019-11-25 22:55:49 +0800

    added navigation items

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Auxiliary from '../../hoc/Auxiliary';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-import classes from './Layout.css'
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
 
-const layout = (props) => (
-  <Auxiliary>
-    <Toolbar></Toolbar>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Auxiliary>
-);
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
 
-export default layout;
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
+
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Auxiliary>
+    )
+  }
+}
+
+export default Layout;
